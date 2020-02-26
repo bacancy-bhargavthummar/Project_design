fa7cc07ed284f60860efd39b828b97b4665a231e
bacancy-bhargavthummar <bhargav.thummar@bacancytechnology.com>
2020-02-26 19:56:58 +0530

    fixed font and js file loading issue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,21 @@
 // that code so it'll be compiled.
 
 import 'bootstrap'
+require("jquery")              //add this in application.js
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
+require('vendor/animsition/js/animsition.min')
+require('vendor/bootstrap/js/popper')
+require('vendor/bootstrap/js/bootstrap.min')
+require('vendor/select2/select2.min.js')
+require('vendor/daterangepicker/moment.min')
+require('vendor/daterangepicker/daterangepicker.js')
+require("vendor/jquery/jquery-3.2.1.min")
+require("vendor/main")
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
